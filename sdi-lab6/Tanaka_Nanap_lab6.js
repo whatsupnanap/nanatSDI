f477671cf8e235c663b3da594e6bf259f4f328af
whatsupnanap <nanat@fullsail.edu>
2014-03-15 23:37:31 -0400

    lab6

---
+++ sdi-lab6/Tanaka_Nanap_lab6.js
@@ -0,0 +1,28 @@
+//alert("JavaScript works!");
+
+var animalsNames = function(){
+	
+	var animalsNames = ["zou", "saru", "kirin", "ushi","usagi","raion"];
+	var inEnglsih = ["Elephant", "monkey", "giraffe", "cow", "bunny","lion"];
+	var newAnimal = "golira";
+	
+	console.log("Let's see all the animals in Japanese and English!");
+	console.log("The length of the array is " + animalsNames.length);
+	
+	for(var i = 0; i < animalsNames.length; i++){
+		
+		console.log("whats " + animalsNames[i] + "?");
+		console.log(animalsNames[i] + " is " + inEnglsih[i] + " in Englsih!");
+	};
+	
+	console.log(animalsNames);
+	console.log("THIS IS HARD ONE! WHAT IS " + newAnimal + "??????????");
+	console.log("Its Gorilla!!");
+	
+	animalsNames.unshift(newAnimal);
+	console.log("this is for today! " + animalsNames);
+	
+	return[animalsNames];
+};
+var test = animalsNames();
+console.log(test);
